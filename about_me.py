50def879e7db04589f8b02525c9036463eec73fe
Programming for Biology <pfb@info06.cshl.edu>
2025-10-14 15:35:34 -0400

    the rest

--- about_me.py
+++ about_me.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 print ("My name: Huishi")
-
+print ("My favorite color: Green")
+fav_activity = "reading"
+print ("My name: Huishi \nMy favorite color: Green", fav_activity)
+print(f"My fav_activity: {fav_activity}")
