7cf30d205ce5819fcc792ae4d2be32d0c83b5300
Programming for Biology <pfb@info06.cshl.edu>
2025-10-15 19:42:37 -0400

    problemset4

--- P4FB_problemsets.py
+++ P4FB_problemsets.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 my_fav_list = ['chocolate', 'husband', 'kid1', 'kid2', 'bed']
 print(my_fav_list)
+print(my_fav_list[2])
+my_fav_list[2] = 'cake'
+print(my_fav_list)
+my_fav_list.append('appended')
+print(my_fav_list)
+my_fav_list.insert(0,'inserted')
+print(my_fav_list)
+my_fav_list.insert(2, 'another')
+print(my_fav_list)
+my_fav_list.pop(7)
+print(my_fav_list)
+my_fav_list.remove('bed')
+print(my_fav_list)
+new_string = ','.join(my_fav_list)
+print(new_string)
