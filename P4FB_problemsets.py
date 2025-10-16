35ee8bd155bd1146bf6563625556e00bff77edec
Programming for Biology <pfb@info06.cshl.edu>
2025-10-15 21:20:47 -0400

    problem6

--- P4FB_problemsets.py
+++ P4FB_problemsets.py
@@ -17,24 +17,56 @@
 # new_string = ','.join(my_fav_list)
 # print(new_string)
 
-taxa_string = 'sapiens : erectus : neanderthalensis'
-print (taxa_string)
-taxa_list = taxa_string.split(':')
-print (taxa_list)
-taxa_list.sort()
-print (taxa_list)
-taxa_list.sort (key = len)
-print (taxa_list)
+# taxa_string = 'sapiens : erectus : neanderthalensis'
+# print (taxa_string)
+# taxa_list = taxa_string.split(':')
+# print (taxa_list)
+# taxa_list.sort()
+# print (taxa_list)
+# taxa_list.sort (key = len)
+# print (taxa_list)
 
-my_list = ['a','bb','ccc']
-# If you use .copy, you don't modify the original list, but if you just assign =, the original list is modified; compare my_list and my_list2.
-list_copy = my_list
-print(my_list)
-list_copy.append('ddd1')
-print(my_list)
-my_list2 = ['a','bb','ccc']
-list_copy2 = my_list2.copy()
-print(my_list2)
-list_copy2.append('ddd2')
-print(my_list2)
+# my_list = ['a','bb','ccc']
+# # If you use .copy, you don't modify the original list, but if you just assign =, the original list is modified; compare my_list and my_list2.
+# list_copy = my_list
+# print(my_list)
+# list_copy.append('ddd1')
+# print(my_list)
+# my_list2 = ['a','bb','ccc']
+# list_copy2 = my_list2.copy()
+# print(my_list2)
+# list_copy2.append('ddd2')
+# print(my_list2)
 
+# number = 1
+# while number < 101:
+#     print("Number:", number)
+#     number +=1
+# print('Done')
+
+# number = 0
+# while number < 100:
+#     number += 1
+#     print("Number:", number)
+# print('Done')
+
+# sum = 0
+# number = 0
+# while number < 101:
+#     sum += number 
+#     number += 1
+# print("Sum:", sum)
+
+# factorial = 1
+# number = 0
+# while number < 10:
+#     number += 1
+#     factorial *= number  
+# print(f'factorial: {factorial}')
+
+even = []
+loopy = [101,2,15,22,95,33,2,27,72,15,52]
+for num in loopy:
+    if num % 2 == 0: 
+        even.append (num)
+print (even)
